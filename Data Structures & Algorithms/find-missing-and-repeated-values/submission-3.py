4f4ecd8321a55ccd17a3a114b3b7d4d3eb89d319
sathish <sathishkumar02052007@gmail.com>
2026-07-07 14:40:31 +0530

    Add: find-missing-and-repeated-values - submission-3

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        a=[]
+        main=[]
+        for i in grid:
+            for j in i:
+                a.append(j)
+        for i in a:
+            if a.count(i)>1:
+                main.append(i)
+                break
+        s=sorted(set(a))
+        for i,j in zip(s,range(1,s[-1]+1)):
+            if i!=j:
+                main.append(j)
+                break   
+        if len(main)==1:
+            main.append(s[-1]+1)       
+        return main             
