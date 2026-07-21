b6e672d9255ce2cf30fe2ded5497699ed6a716e4
sathish <sathishkumar02052007@gmail.com>
2026-07-21 12:43:39 +0530

    Add: take-gifts-from-the-richest-pile - submission-2

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-2.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        i=0
+        while(i<k):
+            maxi=max(gifts)
+            gifts[gifts.index(maxi)]=int(math.sqrt(maxi))
+            i+=1   
+        return sum(gifts)            
+
+        
+        
