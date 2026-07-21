8ea2dce0fc0333557bbc171c0d472d2b92f9d9e0
sathish <sathishkumar02052007@gmail.com>
2026-07-21 14:11:26 +0530

    Add: set-mismatch - submission-33

---
+++ Data Structures & Algorithms/set-mismatch/submission-33.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        a=[]
+        for i in nums:
+           if nums.count(i)>1:
+            a.append(i)
+            break
+        d=sorted(set(nums))    
+        for i,j in zip(nums,range(1,len(nums)+1)):
+            print(i,j)
+            if i!=j:
+                if j not in nums:
+                  a.append(j)
+                  break
+        return a
